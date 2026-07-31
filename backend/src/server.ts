36ace3b46f4855470a55c97d504bd68a37abfb94
wojcho <wojhoo3@gmail.com>
2026-07-31 22:13:36 +0200

    Make frontend usable as served from backend server after build

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,21 @@
 import app from "./app";
 
+import express from "express";
+import path from "path";
+
 const PORT = process.env.PORT || 3000;
 
+// Serve Vite build files
+app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+
+// SPA fallback (React Router)
+app.get("/{*splat}", (req, res) => {
+  res.sendFile(
+    path.join(__dirname, "../../frontend/dist/index.html")
+  );
+});
+
+// Listen application
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
